c32a08b00e6859884153ff331a953054eba5c0cf
daimonix <dimaprotsap@gmail.com>
2022-11-04 22:13:23 +0200

    ~ updated player camera

--- Assets/Scripts/Player/PlayerCamera.cs
+++ Assets/Scripts/Player/PlayerCamera.cs
@@ -5,22 +5,24 @@ namespace Player
     [RequireComponent(typeof(Camera))]
     public class PlayerCamera : MonoBehaviour
     {
-        private Transform _player;
+        [SerializeField] private Transform target;
         public Camera Camera { get; private set; }
+        [SerializeField] private Vector3 offset = new(0f, 0f, -10f);
+        [SerializeField] [Min(0.001f)] private float smoothSpeed = 0.125f;
 
         private void Start()
         {
-            _player = FindObjectOfType<PlayerController>().transform;
             Camera = gameObject.GetComponent<Camera>();
         }
 
         private void LateUpdate()
         {
-            if (!_player) return;
-            // TODO: add camera smoothing
-            var pos = _player.position;
-            pos.z = transform.position.z;
-            transform.position = pos;
+            if(!target) return;
+            Vector3 targetPosition = target.position + offset;
+            Vector3 smoothedPosition = Vector3.Lerp(transform.position, targetPosition, smoothSpeed);
+            transform.position = smoothedPosition;
+
+            transform.LookAt(target);
         }
     }
 }
